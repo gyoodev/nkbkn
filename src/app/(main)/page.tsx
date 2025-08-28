e1652e45c28fbf5211d25437658764be21a11cf6
Gyuray Kamber <gyuraykamber99@gmail.com>
2025-08-28 06:40:58 +0000

    Last changes broke the homepage the navigation is not showing

--- src/app/(main)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getNewsPosts } from '@/lib/data';
-import type { NewsPost } from '@/lib/types';
-import { HomeClientPage } from './components/home-client-page';
-
-export default async function HomePage() {
-  const posts: NewsPost[] = await getNewsPosts();
-
-  return <HomeClientPage posts={posts} />;
-}
