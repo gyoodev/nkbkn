fb4f2c54acd074f9dad067796a6b0ad283128563
Gyuray Kamber <gyuraykamber99@gmail.com>
2025-08-28 17:43:44 +0000

    Replace twitter social icon from Socials with tiktok

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -7,13 +7,32 @@ import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { useLanguage } from '@/hooks/use-language';
 import { PageHeader } from '@/components/page-header';
-import { Twitter, Facebook, Instagram } from 'lucide-react';
+import { Facebook, Instagram } from 'lucide-react';
+
+function TiktokIcon(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            {...props}
+        >
+            <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.28 1.66-1.14 3.1-2.73 3.91-1.61.8-3.39.88-5.04.22-1.63-.65-2.9-1.9-3.48-3.55-.58-1.64-.54-3.43.12-5.02.51-1.29 1.38-2.43 2.48-3.32.79-.65 1.76-1.11 2.78-1.36.01 2.87-.01 5.74.02 8.61.23 1.52 1.04 2.84 2.43 3.58.97.51 2.1.75 3.22.56.83-.13 1.6-.52 2.22-1.11.62-.59.99-1.39 1.12-2.22.12-.82.09-1.68-.08-2.52-.18-1.08-.52-2.11-1.02-3.09-.59-1.16-1.36-2.21-2.3-3.05v-2.88c.01-.13.01-.25.01-.38z" />
+        </svg>
+    )
+}
 
 export default function ContactPage() {
   const { text } = useLanguage();
 
   const socialLinks = [
-    { icon: <Twitter className="h-6 w-6" />, href: '#' },
+    { icon: <TiktokIcon className="h-6 w-6" />, href: '#' },
     { icon: <Facebook className="h-6 w-6" />, href: '#' },
     { icon: <Instagram className="h-6 w-6" />, href: '#' },
   ];
