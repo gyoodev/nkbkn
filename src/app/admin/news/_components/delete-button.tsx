cbaea3a75eda1fc97cfa15d22b9aec5c4b4c802e
Gyuray Kamber <gyuraykamber99@gmail.com>
2025-09-01 04:42:49 +0000

    Също така след дейстие изтрий и успешно изтриване да се появава съобщени

--- src/app/admin/news/_components/delete-button.tsx
+++ src/app/admin/news/_components/delete-button.tsx
@@ -4,13 +4,27 @@
 import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
 import { DeleteNewsPost } from '../actions';
 import { useTransition } from 'react';
+import { useToast } from '@/hooks/use-toast';
 
 export function DeleteButton({ id }: { id: number }) {
   const [isPending, startTransition] = useTransition();
+  const { toast } = useToast();
 
   const handleDelete = () => {
     startTransition(async () => {
-      await DeleteNewsPost(id);
+      const result = await DeleteNewsPost(id);
+      if (result?.message) {
+         toast({
+            variant: 'destructive',
+            title: 'Грешка',
+            description: result.message,
+        });
+      } else {
+        toast({
+            title: 'Успех!',
+            description: 'Успешно изтрихте публикацията.',
+        });
+      }
     });
   };
 
