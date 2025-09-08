880d6a1bb956462cd5589c9ba5178d9597d0694a
Gyuray Kamber <gyuraykamber99@gmail.com>
2025-09-08 07:15:21 +0000

    Use this for social icon Facebook - <svg xmlns="http://www.w3.org/2000/s

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -11,7 +11,7 @@ import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { useLanguage } from '@/hooks/use-language';
 import { PageHeader } from '@/components/page-header';
-import { Facebook, Share2, Loader2 } from 'lucide-react';
+import { Share2, Loader2 } from 'lucide-react';
 import type { SocialLink } from '@/lib/types';
 import { useState } from 'react';
 import { getSocialLinks } from '@/lib/client/data';
@@ -42,10 +42,18 @@ function InstagramIcon(props: React.SVGProps<SVGSVGElement>) {
     )
 }
 
+function FacebookIcon(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" {...props} fill="currentColor">
+            <path d="M576 320C576 178.6 461.4 64 320 64C178.6 64 64 178.6 64 320C64 440 146.7 540.8 258.2 568.5L258.2 398.2L205.4 398.2L205.4 320L258.2 320L258.2 286.3C258.2 199.2 297.6 158.8 383.2 158.8C399.4 158.8 427.4 162 438.9 165.2L438.9 236C432.9 235.4 422.4 235 409.3 235C367.3 235 351.1 250.9 351.1 292.2L351.1 320L434.7 320L420.3 398.2L351 398.2L351 574.1C477.8 558.8 576 450.9 576 320z"/>
+        </svg>
+    )
+}
+
 function SocialIcon({ name, ...props }: { name: SocialLink['name'] } & React.SVGProps<SVGSVGElement>) {
     switch (name) {
         case 'TikTok': return <TiktokIcon {...props} />;
-        case 'Facebook': return <Facebook {...props} />;
+        case 'Facebook': return <FacebookIcon {...props} />;
         case 'Instagram': return <InstagramIcon {...props} />;
         case 'Youtube': return <YoutubeIcon {...props} />;
         default: return <Share2 {...props} />;
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Facebook, Code, Share2 } from 'lucide-react';
+import { Code, Share2 } from 'lucide-react';
 import { useLanguage } from '@/hooks/use-language';
 import Image from 'next/image';
 import type { SocialLink } from '@/lib/types';
@@ -36,10 +36,18 @@ function InstagramIcon(props: React.SVGProps<SVGSVGElement>) {
     )
 }
 
+function FacebookIcon(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" {...props} fill="currentColor">
+            <path d="M576 320C576 178.6 461.4 64 320 64C178.6 64 64 178.6 64 320C64 440 146.7 540.8 258.2 568.5L258.2 398.2L205.4 398.2L205.4 320L258.2 320L258.2 286.3C258.2 199.2 297.6 158.8 383.2 158.8C399.4 158.8 427.4 162 438.9 165.2L438.9 236C432.9 235.4 422.4 235 409.3 235C367.3 235 351.1 250.9 351.1 292.2L351.1 320L434.7 320L420.3 398.2L351 398.2L351 574.1C477.8 558.8 576 450.9 576 320z"/>
+        </svg>
+    )
+}
+
 function SocialIcon({ name, ...props }: { name: SocialLink['name'] } & React.SVGProps<SVGSVGElement>) {
     switch (name) {
         case 'TikTok': return <TiktokIcon {...props} />;
-        case 'Facebook': return <Facebook {...props} />;
+        case 'Facebook': return <FacebookIcon {...props} />;
         case 'Instagram': return <InstagramIcon {...props} />;
         case 'Youtube': return <YoutubeIcon {...props} />;
         default: return <Share2 {...props} />;
