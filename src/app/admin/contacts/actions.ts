a8b4b3cc8bafb0addfe9f7db0f5f60a676426c84
Gyuray Kamber <gyuraykamber99@gmail.com>
2025-09-13 13:37:31 +0000

    Нова грешка при изпращане на имейл от админ панела

--- src/app/admin/contacts/actions.ts
+++ src/app/admin/contacts/actions.ts
@@ -95,11 +95,15 @@ export async function sendEmailReply(prevState: any, formData: FormData): Promis
 
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_SERVER_HOST,
-        port: Number(process.env.EMAIL_SERVER_PORT || 587),
-        secure: Number(process.env.EMAIL_SERVER_PORT) === 465,
+        port: Number(process.env.EMAIL_SERVER_PORT || 465),
+        secure: true, // true for 465, false for other ports
         auth: {
             user: process.env.EMAIL_SERVER_USER,
             pass: process.env.EMAIL_SERVER_PASS,
+        },
+        tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false,
         },
     });
 
